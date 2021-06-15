1a6de0e37678315df4672454a62c8f2bd6d8e1b9
liaof <f.liao163@gmail.com>
2021-06-14 23:51:58 -0400

    18.1

---
+++ README.md
@@ -0,0 +1,48 @@
+### Learning Goals
+- Integrate the API code with the client’s existing codebase.
+- Create a Pizza model using Mongoose.
+- Use Mongoose’s middleware to automate functionality.
+- Implement Mongoose’s pre-built methods for CRUD operations.
+- Complete a working front end to create a new pizza.
+
+- Create a Mongoose model.
+- Set up CRUD methods that use Mongoose models.
+- Create a relationship between two models.
+- Interact with arrays in Mongoose.
+- Work with subdocuments in Mongoose.
+- Implement getters with Mongoose.
+
+MERN stack - MERN stands for MongoDB, Express.js, React, and Node.js
+
+const dogObject = {
+--// this...
+--bark: function() {
+----console.log('Woof!');
+--},
+
+--// ... is the same as this
+--bark() {
+----console.log('Woof!');
+--}
+}
+
+// this code
+router.route('/').get(getCallbackFunction).post(postCallbackFunction);
+
+// is this same as this
+router.get('/', getCallbackFunction);
+router.post('/' postCallbackFunction);
+
+
+- .findOneAndUpdate() find a single document, updates it, and returns. Set 3rd param as { new: true } to return a new document instead of the original
+- .updateOne(), .updateMany() updates documents without returning them
+- .insertOne(), .insertMany() MongoDB methods for adding data to a collection/database
+- .create() Mongoose method for both single and multiple data inserts
+- .deleteOne() .deleteMany() deletes data from collection/database
+- .findOneAndDelete() returns the document to be deleted before deleting it
+
+### Tools Used
+
+- MongoDB
+- Mongoose
+- Express.js
---
+++ controllers/pizza-controller.js
@@ -0,0 +1,64 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // get all pizzas
+    // callback function for GET /api/pizzas route
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // get one pizza by id
+    // note we can not use the where clause as a param in getPizzaById, unlike in Sequelize
+    getPizzaById({ params }, res) {
+        // instead of accessing the entire req, destructure params out of it because it contains all the neccessary data to fufil this request
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                // if no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // createPizza
+    createPizza({ body }, res) {// destructure body out of the Express.js req object again
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+    // update pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new:true })// new:true instructs mongoose to return a new version of the document from the request, instead of the original
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!'});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // delete pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = pizzaController;
