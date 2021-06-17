9b85ffd97dde9a55a5a9dceadb06d4ab08600938
liaof <f.liao163@gmail.com>
2021-06-17 02:58:14 -0400

    -m

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,7 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+  console.log('fetching pizza POST')
   fetch('/api/pizzas', {
     method: 'POST',
     headers: {
